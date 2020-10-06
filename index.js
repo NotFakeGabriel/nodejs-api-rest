ec7acf87d40cbbd9fd7995c4c4063182e31e6755
NotFakeGabriel <gabriel@LAPTOP-FKLPRDKF.localdomain>
2020-10-06 19:13:20 -0300

    Create infra and db connection

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //funçao de subir o servidor
 const customExpress = require('./config/customExpress') 
+const conexao = require('./infra/conexao')
 
-const app = customExpress()
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
+        
+        const app = customExpress()
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+
+    }
+})
 
